e40033be2a9fe25b240efbadbd85c2b8a331cd5d
SchrodingerZhu <i@zhuyi.fan>
2020-10-19 18:03:43 +0800

    type encode

--- include/pika/memotable.hpp
+++ include/pika/memotable.hpp
@@ -10,15 +10,50 @@
 #define PIKA_INLINE_MATCHED 7
 namespace pika {
     namespace memotable {
+        namespace _internal {
+            enum class BaseType {
+                Seq,
+                Ord,
+                OneOrMore,
+                FollowedBy,
+                NotFollowedBy,
+                Plus,
+                Char,
+                CharRange,
+                First,
+                Nothing,
+                Any,
+                Error
+            };
+#define PIKA_CHECK_BASE(NAMESPACE, TYPE) \
+                 if constexpr(std::is_base_of_v<NAMESPACE::TYPE, std::decay_t<Clause>>) \
+                 { return BaseType::TYPE; }
+
+            template<class Clause, typename = std::enable_if_t<std::is_base_of_v<pika::clause::Clause, Clause>>>
+            constexpr BaseType get_base_type() {
+                PIKA_CHECK_BASE(pika::clause::_internal, Seq)
+                else PIKA_CHECK_BASE(pika::clause::_internal, Ord)
+                else PIKA_CHECK_BASE(pika::clause::_internal, OneOrMore)
+                else PIKA_CHECK_BASE(pika::clause::_internal, FollowedBy)
+                else PIKA_CHECK_BASE(pika::clause::_internal, NotFollowedBy)
+                else PIKA_CHECK_BASE(pika::clause::_internal, Plus)
+                else PIKA_CHECK_BASE(pika::clause::_internal, Char)
+                else PIKA_CHECK_BASE(pika::clause::_internal, CharRange)
+                else PIKA_CHECK_BASE(pika::clause, First)
+                else PIKA_CHECK_BASE(pika::clause, Nothing)
+                else PIKA_CHECK_BASE(pika::clause, Any)
+                else return BaseType::Error;
+            }
+        }
+#undef PIKA_CHECK_BASE
         struct MemoKey {
             const std::type_index clause_type;
             const size_t start_position;
-            const pika::clause::Clause  * const tag;
+            const _internal::BaseType base_type;
 
             template<class Clause, typename = std::enable_if_t<std::is_base_of_v<pika::clause::Clause, Clause>>>
-            MemoKey(const Clause &tag, size_t start_position) noexcept : tag(&tag), start_position(start_position),
-                                                                         clause_type(typeid(Clause)) {
-            }
+            MemoKey(const Clause &tag, size_t start_position) noexcept : base_type(_internal::get_base_type<Clause>()), start_position(start_position),
+                                                                         clause_type(typeid(Clause)) {}
 
             template<typename H>
             friend H AbslHashValue(H h, const MemoKey &k) {
@@ -32,7 +67,7 @@ namespace pika {
 
             template<class C>
             [[nodiscard]] bool is_based_on() const noexcept {
-                return dynamic_cast<const C *>(tag) != nullptr;
+                return _internal::get_base_type<C>() == base_type;
             }
         };
 
