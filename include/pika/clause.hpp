50f6d49d215ff61122f699ccaf03f64285379357
SchrodingerZhu <i@zhuyi.fan>
2020-10-19 17:30:44 +0800

    prototypes

--- include/pika/clause.hpp
+++ include/pika/clause.hpp
@@ -6,75 +6,98 @@
 namespace pika {
 
     namespace clause {
+
         struct Clause {
-
+            virtual ~Clause() = default;
         };
 
-        struct Terminal : public Clause {
+        namespace _internal {
+            struct Terminal : public Clause {
 
-        };
-        struct NonTerminal : public Clause {
+            };
+            struct NonTerminal : public Clause {
 
+            };
+
+            struct Char : public Terminal {};
+            struct CharRange : public Terminal {};
+            struct Seq : public NonTerminal {};
+            struct Ord : public NonTerminal {};
+            struct Plus : public NonTerminal {};
+            struct OneOrMore : public NonTerminal {};
+            struct FollowedBy : public NonTerminal {};
+            struct NotFollowedBy : public NonTerminal {};
         };
 
         template<char C>
-        struct Char : public Terminal {
+        struct Char : public _internal::Char {
 
         };
 
         template<char Start, char End>
-        struct CharRange : public Terminal {
+        struct CharRange : _internal::CharRange {
 
         };
 
-        struct Nothing : public Terminal {
+        struct First : public _internal::Terminal {
 
         };
 
-        struct Any : public Terminal {
+        struct Nothing : public _internal::Terminal {
 
         };
 
-        template<typename H, typename ...T>
-        struct Seq : private H, private Seq<T...> {
-
-        };
-
-        template<typename H>
-        struct Seq<H> : private H, public NonTerminal {
+        struct Any : public _internal::Terminal {
 
         };
 
         template<typename H, typename ...T>
-        struct Ord : private H, private Ord<T...> {
+        struct Seq : public Seq<T...> {
 
         };
 
         template<typename H>
-        struct Ord<H> : private H, public NonTerminal {
+        struct Seq<H> : public _internal::Seq {
+
+        };
+
+
+
+        template<typename H, typename ...T>
+        struct Ord : public Ord<T...> {
+
+        };
+
+        template<typename H>
+        struct Ord<H> : public _internal::Ord {
+
+        };
+
+
+        template<typename S>
+        struct Plus : public _internal::Plus {
+
+        };
+
+
+        template<typename S>
+        struct OneOrMore : public _internal::OneOrMore {
+
+        };
+
+
+        template<typename S>
+        struct FollowedBy : public _internal::FollowedBy {
 
         };
 
         template<typename S>
-        struct Plus : public NonTerminal {
+        struct NotFollowedBy : public _internal::NotFollowedBy {
 
         };
 
-        template<typename S>
-        struct OneOrMore : public NonTerminal {
-
-        };
-
-        template<typename S>
-        struct FollowedBy : public NonTerminal {
-
-        };
-
-        template<typename S>
-        struct NotFollowedBy : public NonTerminal {
-
-        };
-
+        template<const char* label>
+        struct Label {};
     }
 } // namespace pika::clause
 #endif //PIKA_CLAUSE_HPP
--- include/pika/memotable.hpp
+++ include/pika/memotable.hpp
@@ -1,25 +1,71 @@
 #ifndef PIKA_MEMOTABLE_HPP
 #define PIKA_MEMOTABLE_HPP
+
 #include <pika/clause.hpp>
 #include <absl/container/flat_hash_map.h>
 #include <typeindex>
+#include <utility>
+
+
+#define PIKA_INLINE_MATCHED 7
 namespace pika {
     namespace memotable {
         struct MemoKey {
             const std::type_index clause_type;
-            size_t start_position;
+            const size_t start_position;
+            const pika::clause::Clause  * const tag;
+
             template<class Clause, typename = std::enable_if_t<std::is_base_of_v<pika::clause::Clause, Clause>>>
-            MemoKey(const Clause&,  size_t start_position) : start_position(start_position), clause_type(typeid(Clause)) {
+            MemoKey(const Clause &tag, size_t start_position) noexcept : tag(&tag), start_position(start_position),
+                                                                         clause_type(typeid(Clause)) {
             }
 
-            template <typename H>
-            friend H AbslHashValue(H h, const MemoKey& k) {
+            template<typename H>
+            friend H AbslHashValue(H h, const MemoKey &k) {
                 return H::combine(std::move(h), k.clause_type, k.start_position);
             }
 
-            bool operator==(const MemoKey& that) const {
+            bool operator==(const MemoKey &that) const noexcept {
                 return that.start_position == start_position &&
-                    that.clause_type == clause_type;
+                       that.clause_type == clause_type;
+            }
+
+            template<class C>
+            [[nodiscard]] bool is_based_on() const noexcept {
+                return dynamic_cast<const C *>(tag) != nullptr;
+            }
+        };
+
+        class Match {
+            const MemoKey key;
+            const size_t length;
+            const size_t sub_fst_idx;
+            const std::vector<std::shared_ptr<Match>> sub_matches;
+        public:
+
+            Match(MemoKey key, size_t length, size_t sub_fst_idx, std::vector<std::shared_ptr<Match>> sub_matches)
+                    : key(std::move(key)), length(length), sub_fst_idx(sub_fst_idx),
+                      sub_matches(std::move(sub_matches)) {}
+
+            std::vector<std::pair<std::string_view, Match>>
+            get_sub_matches() {
+                throw std::runtime_error("unimplemented"); /// TODO: implement this
+                return this->get_sub_matches();
+            }
+
+            bool is_better_than(const Match& that) {
+                if (&that == this) {
+                    return false;
+                }
+                return (key.is_based_on<pika::clause::First>()
+                        && this->sub_fst_idx < that.sub_fst_idx)
+                        || this->length > that.length;
+            }
+        };
+
+        class MemoTable : public absl::flat_hash_map<MemoKey, Match> {
+            MemoTable() : absl::flat_hash_map<MemoKey, Match>() {
+
             }
         };
     }
